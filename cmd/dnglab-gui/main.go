668b80f01dcdf747f095e15fdba1b4d43a53d3b8
Juha Jantunen <juha@strobotti.com>
2026-09-20 09:44:46 +0300

    feat: integrate application icon using fyne bundle
    - Bundle assets/icon.png into internal/assets/bundled.go
    - Add internal/assets/icon.go with AppIcon() getter function
    - Set app icon in main.go using a.SetIcon(assets.AppIcon())

--- cmd/dnglab-gui/main.go
+++ cmd/dnglab-gui/main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"fyne.io/fyne/v2/app"
+	"github.com/strobotti/dnglab-gui/internal/assets"
 	"github.com/strobotti/dnglab-gui/internal/ui"
 )
 
 func main() {
 	a := app.NewWithID("com.github.strobotti.dnglab-gui")
+	a.SetIcon(assets.AppIcon())
 	w := ui.NewMainWindow(a)
 	w.ShowAndRun()
 }
---
+++ internal/assets/icon.go
@@ -0,0 +1,8 @@
+package assets
+
+import "fyne.io/fyne/v2"
+
+// AppIcon returns the application icon resource.
+func AppIcon() fyne.Resource {
+	return resourceIconPng
+}
